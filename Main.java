4c2f8f1ebf5499a6d266148dc560055925000fa3
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-13 13:30:07 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * tells user what their letter grade was
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+  // takes the percentage and gives the user their letter
+  public static void examMark(int examPercent) {
+      if (examPercent > 79) {
+        System.out.println("You got an A");
+      } else if (examPercent > 69) {
+        System.out.println("You got a B");
+      } else if (examPercent > 59) {
+        System.out.println("You got a C");
+      } else if (examPercent > 49) {
+        System.out.println("You got a D");
+      } else {
+        System.out.println("You got an F");
+      }     
+  }
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    // ask what their percentage was
+    System.out.println("What percentage did you get on your exam?");
+    int examPercent = input.nextInt();
+
+    // tell them their letter grade
+    examMark(examPercent);
   }
 }
